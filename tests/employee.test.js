ea1c8b141b975ad43bf886a171a15d0150e4e4d3
Jarod May <jarodmay@jarods-mbp-2.lan>
2021-11-08 20:53:02 -0500

    Added .gitignore information. Added HTML code/framework. Started working through test and subsequent errors, part of the difficultiy was getting the tests setup to run at all. Did NPM installs during this stage as well

---
+++ tests/employee.test.js
@@ -0,0 +1,37 @@
+
+const employee = require('../lib/employee.js');
+
+test('creates an employee object', () => {
+    const Employee = new employee('Nicole', 90, 'nicole.elisaw@gmail.com');
+
+    expect(Employee.name).toEqual(expect.any(String));
+    expect(Employee.id).toEqual(expect.any(Number));
+    expect(Employee.email).toEqual(expect.any(String));
+});
+
+
+test('gets employee name', () => {
+    const Employee = new employee('Nicole', 90, 'nicole.elisaw@gmail.com');
+
+    expect(Employee.getName()).toEqual(expect.any(String));
+});
+
+test('gets employee ID', () => {
+    const Employee = new employee('Nicole', 90, 'nicole.elisaw@gmail.com');
+
+    expect(Employee.getId()).toEqual(expect.any(Number));
+});
+
+
+test('gets employee email', () => {
+    const Employee = new employee('Nicole', 90, 'nicole.elisaw@gmail.com');
+
+    expect(Employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+
+test('gets role of employee', () => {
+    const Employee = new employee('Nicole', 90, 'nicole.elisaw@gmail.com');
+
+    expect(employee.getRole()).toEqual("Employee");
+}); 
